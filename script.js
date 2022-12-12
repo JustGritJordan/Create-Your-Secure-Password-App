f5206e07ea49d12ace82a4888437a29c3fff26dc
JGJ <jordan.gb0@gmail.com>
2022-12-11 23:33:56 -0500

    started arrays

--- script.js
+++ script.js
@@ -15,33 +15,53 @@ generateBtn.addEventListener("click", writePassword);
 // THEN I select which criteria to include in the password
 
 function generatePassword() {
-  var passwordLength = prompt(
-    "Enter a password no less than 8 characters and no more than 128 characters"
-  );
-
+    var password = [
+        var lowerPrompt = ("abcdefghijklmnopqrstuzwxyz"); 
+    ]
   //Step 3:
   // WHEN prompted for the length of the password
   // THEN I choose a length of at least 8 characters and no more than 128 characters
-
-  var lowerPrompt = prompt("Do you want a lowercase character?");
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("invalid");
+  var passwordLength = prompt(
+    "Enter a password length no less than 8 characters and no more than 128 characters"
+  );
+  if (passwordLength > 8 || passwordLength < 128) {
+    alert("Recevied");
+  } else {
+    alert("Will not be included");
   }
-
   //Step 4:
   // WHEN asked for character types to include in the password
   // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 
+  var lowerPrompt = prompt("Do you want a lowercase character, yes or no?");
+  if (lowerPrompt === "yes") {
+    alert("Recieved");
+  } else {
+    alert("Will not be included");
+  }
+
   var upperPrompt = prompt("Do you want a uppercase character, yes or no?");
   if (upperPrompt === "yes") {
+    alert("Recieved");
+  } else {
+    alert("Will not be included");
+
     // Select and uppercase character from an array
   }
   var numberPrompt = prompt("Do you want a number character, yes or no?");
   if (numberPrompt === "yes") {
+    alert("Recieved");
+  } else {
+    alert("Will not be included");
+
     // Select and number character from an array
   }
   var specialPrompt = prompt("Do you want a special character, yes or no?");
   if (specialPrompt === "yes") {
+    alert("Recieved");
+  } else {
+    alert("Will not be included");
+
     // Select and uppercase character from an array
   }
 
@@ -64,6 +84,8 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+var passwordLength;
 
 // created empty results to store usuer arrays
 // const results[
