e90a897f5fd46f0053ec86b11044e6f3ebe0abc1
JGJ <jordan.gb0@gmail.com>
2022-12-31 13:43:14 -0500

    completed radmonization code, moving on to next step in notes

--- script.js
+++ script.js
@@ -1,141 +1,133 @@
-// GIVEN I need a new, secure password
-// Assignment code here
+// // GIVEN I need a new, secure password
+// // Assignment code here
 
-//Step 1:
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
+// //Step 1:
+// // WHEN I click the button to generate a password
+// // THEN I am presented with a series of prompts for password criteria
+// var generateBtn = document.querySelector("#generate");
+// generateBtn.addEventListener("click", writePassword);
 
-// Get references to the #generate element
-// Add event listener to generate button
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
+// function generatePassword() {
+//   var passwordLength = prompt(
+//     "Enter a password length no less than 8 characters and no more than 128 characters"
+//   );
+//   if (passwordLength > 8 && passwordLength < 128) {
+//     alert("Recevied");
+//   } else {
+//     alert("Invalid Input");
+//   }
+//   // -------------------------------------------------------------------------------------
+//   var lowerPrompt = prompt("Do you want a lowercase character, yes or no?");
 
-//Step 2:
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
+//   if (lowerPrompt === "yes") {
+// var lowerLetterChar = [
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+//   "g",
+//   "h",
+//   "i",
+//   "j",
+//   "k",
+//   "l",
+//   "m",
+//   "n",
+//   "o",
+//   "p",
+//   "q",
+//   "r",
+//   "s",
+//   "t",
+//   "u",
+//   "v",
+//   "w",
+//   "x",
+//   "y",
+//   "z",
+// ];
+// var randomLowerLetter = Math.floor(Math.random() * lowerLetterChar.length);
+// console.log(lowerLetterChar[randomLowerLetter]);
 
-function generatePassword() {
-  //Step 3:
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  var passwordLength = prompt(
-    "Enter a password length no less than 8 characters and no more than 128 characters"
-  );
-  if (passwordLength > 8 || passwordLength < 128) {
-    alert("Recevied");
-  } else {
-    alert("Will not be included");
-  }
-  //Step 4:
-  // WHEN asked for character types to include in the password
-  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// for (var i = 0; i < lowerLetterChar.length; i = i + 1) {
+//   console.log(lowerLetterChar[i]);
+// }
+//
 
-  var lowerPrompt = prompt("Do you want a lowercase character, yes or no?");
-  if (lowerPrompt === "yes") {
-    alert("Recieved");
-  } else {
-    alert("Will not be included");
-  }
+//     alert("Recieved");
+//     // Place for randamization of lower letter charters array
+//   } else {
+//     alert("Will not be included");
+//   }
 
-  var upperPrompt = prompt("Do you want a uppercase character, yes or no?");
-  if (upperPrompt === "yes") {
-    alert("Recieved");
-  } else {
-    alert("Will not be included");
-  }
-  var numberPrompt = prompt("Do you want a number character, yes or no?");
-  if (numberPrompt === "yes") {
-    alert("Recieved");
-  } else {
-    alert("Will not be included");
-  }
-  var specialPrompt = prompt("Do you want a special character, yes or no?");
-  if (specialPrompt === "yes") {
-    alert("Recieved");
-  } else {
-    alert("Will not be included");
-  }
+//   var upperPrompt = prompt("Do you want a uppercase character, yes or no?");
+//   if (upperPrompt === "yes") {
+//     alert("Recieved");
+//   } else {
+//     alert("Will not be included");
+//   }
+//   var numberPrompt = prompt("Do you want a number character, yes or no?");
+//   if (numberPrompt === "yes") {
+//     alert("Recieved");
+//   } else {
+//     alert("Will not be included");
+//   }
+//   var specialPrompt = prompt("Do you want a special character, yes or no?");
+//   if (specialPrompt === "yes") {
+//     alert("Recieved");
+//   } else {
+//     alert("Will not be included");
+//   }
+// }
 
-  //Step 5:
-  // WHEN I answer each prompt
-  // THEN my input should be validated and at least one character type should be selected
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  //Step 6:
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
-}
+//   passwordText.value = password;
+// }
 
-//Step 7:
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// var upperCaseLetters = [
+//   "A",
+//   "B",
+//   "C",
+//   "D",
+//   "E",
+//   "F",
+//   "G",
+//   "H",
+//   "I",
+//   "J",
+//   "K",
+//   "L",
+//   "M",
+//   "N",
+//   "O",
+//   "P",
+//   "Q",
+//   "R",
+//   "S",
+//   "T",
+//   "U",
+//   "V",
+//   "W",
+//   "X",
+//   "Y",
+//   "Z",
+// ];
+// var randomUpperLetter = Math.floor(Math.random() * upperCaseLetters.length);
 
-  passwordText.value = password;
-}
+// var passwordLength;
 
-var passwordLength;
+// var numbers = ["0", "1", "2", "3", "2", "4", "5", "6", "7", "8", "9"];
+// var randomNumber = Math.floor(Math.random() * numbers.length);
+// console.log(numbers[randomNumber]);
+//
+// var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+// var randomSpecialChar = Math.floor(Math.random() * specialChar.length);
+// console.log(specialChar[randomSpecialChar]);
 
-var lowerPrompt = [
-  "a",
-  "b,",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var upperPrompt = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var numberPrompt = ["0", "1", "2", "3", "2", "4", "5", "6", "7", "8", "9"];
-var specialPrompt = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
-var passwordSelections = [];
-// created empty results to store usuer arrays
+// var passwordSelections = [];
+// // created empty results to store usuer arrays
